b7eec6b614370ed5da5b0b3b942ded58aeb884ea
goldenacrebc <goldenacrebc@hotmail.com>
2026-09-24 15:16:29 +0100

    Update modals.js

--- modals.js
+++ modals.js
@@ -130,15 +130,15 @@ const modalData = {
         tag: "internal",
         title: "Competition Winners",
         body: `
-            <p><strong>2026 Winners</strong></p>
-            <p>Gents Championship - <strong>Malcolm Thomson</strong></p>
-            <p>Ladies Championship - <strong>Rita Reid</strong></p>
-            <p>Junior Championship - <strong>Daniel Taylor</strong></p>
-            <p>Gents 2 Bowl - <p>
+            <p><strong><u>2026 Winners</u></strong></p>
+            <p><strong>Gents Championship</strong> - Malcolm Thomson/p>
+            <p><strong>Ladies Championship </strong> - Rita Reid</p>
+            <p><strong>Junior Championship </strong> - Daniel Taylor</p>
+            <p><strong>Gents 2 Bowl</strong> - <p>
             <hr>
-            <p>Gents Pairs - </p>
-            <p>Gents Triples - <strong>George Lamb, Daniel Taylor, Billy Taylor</strong></p>
-            <p>Gents Rinks - </p>
+            <p><strong>Gents Pairs</strong> - </p>
+            <p><strong>Gents Triples </strong> - George Lamb, Daniel Taylor, Billy Taylor</p>
+            <p><strong>Gents Rinks </strong> - </p>
             <hr>
             <p>More Details to follow after closing day</p>
         `
